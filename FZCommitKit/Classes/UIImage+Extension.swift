71aafa7eaa5c411b567c8ff8811619e37c5876d9
Jack <xinxiang@xinxiangdeMacBook-Pro.local>
2022-01-23 11:39:33 +0800

    0.0.5

--- FZCommitKit/Classes/UIImage+Extension.swift
+++ FZCommitKit/Classes/UIImage+Extension.swift
@@ -141,96 +141,96 @@ extension UIImage {
 }
 
 
-extension UIImage {
-    ///图片压缩限制大小，压缩图片质量（二分压缩法，压缩图片质量的优点在于，尽可能保留图片清晰度，图片不会明显模糊；缺点在于，图片过大时压到一定程度将不会再压缩，因此不能保证图片压缩后小于指定大小）
-    /// - Parameter maxLength: 最大长度byte字节（比如限制1M大小，传入1024*1024）
-    /// - Returns: 图片数据
-    open func fz_CompressByQuality(maxLength: Int) -> UIImage {
-        var compression: CGFloat = 1
-        guard var data = self.jpegData(compressionQuality: compression), data.count > maxLength else { return self }
-        FZLog("压缩前kb: \( Double((data.count)/1024))")
-        var max: CGFloat = 1
-        var min: CGFloat = 0
-        for _ in 0..<6 {
-            compression = (max + min) / 2
-            data = self.jpegData(compressionQuality: compression) ?? Data()
-            if CGFloat(data.count) < CGFloat(maxLength) * 0.9 {
-                min = compression
-            } else if data.count > maxLength {
-                max = compression
-            } else {
-                break
-            }
-        }
-        FZLog("压缩后kb: \( Double((data.count)/1024))")
-        let resultImage: UIImage = UIImage(data: data) ?? self
-        return resultImage
-    }
-    
-    ///图片压缩限制大小，压缩图片尺寸（压缩图片尺寸可以使图片小于指定大小，但会使图片明显模糊）
-    /// - Parameter maxLength: 最大长度byte字节（比如限制1M大小，传入1024*1024）
-    /// - Returns: 图片数据
-    open func fz_CompressBySize(maxLength: Int) -> UIImage {
-        var resultImage = self
-        var lastDataLength: Int = 0
-        guard var data = self.jpegData(compressionQuality: 1), data.count > maxLength else { return self }
-        debugPrint("压缩前kb: \( Double((data.count)/1024))")
-        while data.count > maxLength && data.count != lastDataLength {
-            lastDataLength = data.count
-            let ratio: CGFloat = CGFloat(maxLength) / CGFloat(data.count)
-            let size: CGSize = CGSize(width: Int(resultImage.size.width * sqrt(ratio)),
-                                      height: Int(resultImage.size.height * sqrt(ratio)))
-            UIGraphicsBeginImageContext(size)
-            resultImage.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-            resultImage = UIGraphicsGetImageFromCurrentImageContext() ?? self
-            UIGraphicsEndImageContext()
-            data = resultImage.jpegData(compressionQuality: 1) ?? Data()
-        }
-        FZLog("压缩后kb: \( Double((data.count)/1024))")
-        return resultImage
-    }
-    
-    /// 图片压缩限制大小，压缩图片质量+尺寸（先压缩图片质量，如果已经小于指定大小，就可得到清晰的图片，否则再压缩图片尺寸）
-    /// - Parameter maxLength: 最大长度byte字节（比如限制1M大小，传入1024*1024）
-    /// - Returns: 图片数据
-    open func fz_Compress(maxLength: Int) -> UIImage {
-        // 压缩质量
-        var compression: CGFloat = 1
-        guard var data = self.jpegData(compressionQuality: compression), data.count > maxLength else { return self }
-        FZLog("压缩前kb: \( Double((data.count)/1024))")
-        var max: CGFloat = 1
-        var min: CGFloat = 0
-        for _ in 0..<6 {
-            compression = (max + min) / 2
-            data = self.jpegData(compressionQuality: compression) ?? Data()
-            if CGFloat(data.count) < CGFloat(maxLength) * 0.9 {
-                min = compression
-            } else if data.count > maxLength {
-                max = compression
-            } else {
-                break
-            }
-        }
-        var resultImage: UIImage = UIImage(data: data) ?? self
-        if data.count < maxLength {
-            FZLog("压缩后kb: \( Double((data.count)/1024))")
-            return resultImage
-        }
-        
-        // 压缩尺寸
-        var lastDataLength: Int = 0
-        while data.count > maxLength && data.count != lastDataLength {
-            lastDataLength = data.count
-            let ratio: CGFloat = CGFloat(maxLength) / CGFloat(data.count)
-            let size: CGSize = CGSize(width: Int(resultImage.size.width * sqrt(ratio)),
-                                      height: Int(resultImage.size.height * sqrt(ratio)))
-            UIGraphicsBeginImageContext(size)
-            resultImage.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-            resultImage = UIGraphicsGetImageFromCurrentImageContext() ?? self
-            UIGraphicsEndImageContext()
-            data = resultImage.jpegData(compressionQuality: compression) ?? Data()
-        }
-        FZLog("压缩后kb: \( Double((data.count)/1024))")
-        return resultImage
-    }
-}
+//extension UIImage {
+//    ///图片压缩限制大小，压缩图片质量（二分压缩法，压缩图片质量的优点在于，尽可能保留图片清晰度，图片不会明显模糊；缺点在于，图片过大时压到一定程度将不会再压缩，因此不能保证图片压缩后小于指定大小）
+//    /// - Parameter maxLength: 最大长度byte字节（比如限制1M大小，传入1024*1024）
+//    /// - Returns: 图片数据
+//    open func fz_CompressByQuality(maxLength: Int) -> UIImage {
+//        var compression: CGFloat = 1
+//        guard var data = self.jpegData(compressionQuality: compression), data.count > maxLength else { return self }
+//        FZLog("压缩前kb: \( Double((data.count)/1024))")
+//        var max: CGFloat = 1
+//        var min: CGFloat = 0
+//        for _ in 0..<6 {
+//            compression = (max + min) / 2
+//            data = self.jpegData(compressionQuality: compression) ?? Data()
+//            if CGFloat(data.count) < CGFloat(maxLength) * 0.9 {
+//                min = compression
+//            } else if data.count > maxLength {
+//                max = compression
+//            } else {
+//                break
+//            }
+//        }
+//        FZLog("压缩后kb: \( Double((data.count)/1024))")
+//        let resultImage: UIImage = UIImage(data: data) ?? self
+//        return resultImage
+//    }
+//
+//    ///图片压缩限制大小，压缩图片尺寸（压缩图片尺寸可以使图片小于指定大小，但会使图片明显模糊）
+//    /// - Parameter maxLength: 最大长度byte字节（比如限制1M大小，传入1024*1024）
+//    /// - Returns: 图片数据
+//    open func fz_CompressBySize(maxLength: Int) -> UIImage {
+//        var resultImage = self
+//        var lastDataLength: Int = 0
+//        guard var data = self.jpegData(compressionQuality: 1), data.count > maxLength else { return self }
+//        debugPrint("压缩前kb: \( Double((data.count)/1024))")
+//        while data.count > maxLength && data.count != lastDataLength {
+//            lastDataLength = data.count
+//            let ratio: CGFloat = CGFloat(maxLength) / CGFloat(data.count)
+//            let size: CGSize = CGSize(width: Int(resultImage.size.width * sqrt(ratio)),
+//                                      height: Int(resultImage.size.height * sqrt(ratio)))
+//            UIGraphicsBeginImageContext(size)
+//            resultImage.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+//            resultImage = UIGraphicsGetImageFromCurrentImageContext() ?? self
+//            UIGraphicsEndImageContext()
+//            data = resultImage.jpegData(compressionQuality: 1) ?? Data()
+//        }
+//        FZLog("压缩后kb: \( Double((data.count)/1024))")
+//        return resultImage
+//    }
+//
+//    /// 图片压缩限制大小，压缩图片质量+尺寸（先压缩图片质量，如果已经小于指定大小，就可得到清晰的图片，否则再压缩图片尺寸）
+//    /// - Parameter maxLength: 最大长度byte字节（比如限制1M大小，传入1024*1024）
+//    /// - Returns: 图片数据
+//    open func fz_Compress(maxLength: Int) -> UIImage {
+//        // 压缩质量
+//        var compression: CGFloat = 1
+//        guard var data = self.jpegData(compressionQuality: compression), data.count > maxLength else { return self }
+//        FZLog("压缩前kb: \( Double((data.count)/1024))")
+//        var max: CGFloat = 1
+//        var min: CGFloat = 0
+//        for _ in 0..<6 {
+//            compression = (max + min) / 2
+//            data = self.jpegData(compressionQuality: compression) ?? Data()
+//            if CGFloat(data.count) < CGFloat(maxLength) * 0.9 {
+//                min = compression
+//            } else if data.count > maxLength {
+//                max = compression
+//            } else {
+//                break
+//            }
+//        }
+//        var resultImage: UIImage = UIImage(data: data) ?? self
+//        if data.count < maxLength {
+//            FZLog("压缩后kb: \( Double((data.count)/1024))")
+//            return resultImage
+//        }
+//
+//        // 压缩尺寸
+//        var lastDataLength: Int = 0
+//        while data.count > maxLength && data.count != lastDataLength {
+//            lastDataLength = data.count
+//            let ratio: CGFloat = CGFloat(maxLength) / CGFloat(data.count)
+//            let size: CGSize = CGSize(width: Int(resultImage.size.width * sqrt(ratio)),
+//                                      height: Int(resultImage.size.height * sqrt(ratio)))
+//            UIGraphicsBeginImageContext(size)
+//            resultImage.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+//            resultImage = UIGraphicsGetImageFromCurrentImageContext() ?? self
+//            UIGraphicsEndImageContext()
+//            data = resultImage.jpegData(compressionQuality: compression) ?? Data()
+//        }
+//        FZLog("压缩后kb: \( Double((data.count)/1024))")
+//        return resultImage
+//    }
+//}
